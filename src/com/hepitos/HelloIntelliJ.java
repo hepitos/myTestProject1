b8c6ff601df97101da10d9caa6e9a898e3edfd4e
hepitos <hepitos@gmail.com>
2019-08-25 15:36:08 +0900

    테스트 commit

---
+++ src/com/hepitos/HelloIntelliJ.java
@@ -0,0 +1,13 @@
+package com.hepitos;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloIntelliJ {
+    public static void main (String[] args) throws Exception {
+        String name = null;
+        System.out.println("Hellow 인텔리제이!!!");
+        System.out.println(StringUtils.isEmpty(name));
+
+    }
+
+}
